4b1f6c03afd36734e6eea2740ac345c10d36cadb
xiawang1024 <xiawang1024@gmail.com>
2020-07-16 17:10:30 +0800

    ✨feat(article): add article page

--- pages/article/[articleId].js
+++ pages/article/[articleId].js
@@ -1,3 +1,53 @@
-export default function ArticleDetail() {
-  return <h1>Article Detail</h1>
+const DEMO_URL =
+  'https://pubmob.dianzhenkeji.com/cms/articles?tenantId=henanradio&channelId=1216567500624498688&pageNo=1&pageSize=50'
+const DETAIL_URL =
+  'https://pubmob.dianzhenkeji.com/cms/articlewithrelated?tenantId=henanradio&articleId='
+
+export default function ArticleDetail({ data }) {
+  if (!data) {
+    return false
+  }
+  return (
+    <div>
+      <h1 className="title">{data.articleTitle}</h1>
+      <div
+        className="rich-text"
+        dangerouslySetInnerHTML={{ __html: data.contentBody }}
+      ></div>
+      <style jsx>{`
+        .title {
+          line-height: 1.6;
+          font-size: 24px;
+          font-weight: bold;
+        }
+        .rich-text {
+          width: 100%;
+          line-height: 1.75;
+          font-size: 18px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(DEMO_URL)
+  const { result } = await res.json()
+  const paths = result.content.map((item) => ({
+    params: { articleId: item.id },
+  }))
+  return {
+    paths: paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`${DETAIL_URL}${params.articleId}`)
+  const { result } = await res.json()
+  return {
+    props: {
+      data: result,
+    },
+  }
 }
--- pages/article/index.js
+++ pages/article/index.js
@@ -1,13 +1,26 @@
+import Link from 'next/link'
+
+const DEMO_URL =
+  'https://pubmob.dianzhenkeji.com/cms/articles?tenantId=henanradio&channelId=1216567500624498688&pageNo=1&pageSize=50'
 export default function Article({ data }) {
   return (
     <ul>
       {data.map((item) => (
-        <li key={item.id}>{item.contentTitle}</li>
+        <li key={item.id}>
+          <Link
+            className="link"
+            href="/article/[articleId]"
+            as={`/article/${item.id}`}
+          >
+            <a>{item.contentTitle}</a>
+          </Link>
+        </li>
       ))}
       <style jsx>{`
         li {
           line-height: 1.5;
           margin-bottom: 20px;
+          font-size: 22px;
         }
       `}</style>
     </ul>
@@ -15,9 +28,7 @@ export default function Article({ data }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(
-    'https://pubmob.dianzhenkeji.com/cms/articles?tenantId=henanradio&channelId=1216567500624498688&pageNo=1&pageSize=20'
-  )
+  const res = await fetch(DEMO_URL)
   const { result } = await res.json()
   return {
     props: {
