ca7165d407a55c8146310c3fb52cf155e81d2008
xiawang1024 <xiawang1024@gmail.com>
2020-07-16 10:59:57 +0800

    🐳chore(eslint): props rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
