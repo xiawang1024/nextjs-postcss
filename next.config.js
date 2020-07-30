dcc077eef05256c2fdabff4270ac8d83881d92d1
xiawang1024 <xiawang1024@gmail.com>
2020-07-30 15:40:30 +0800

    🐞fix(next.config.js): 静态资源路径

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 // 生产环境可以把 .next/static 下静态资源上传到静态资源服务器
 module.exports = {
-  //   assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
+  assetPrefix: isProd ? '' : '',
 }
